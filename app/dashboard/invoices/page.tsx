fc5517b5af09b17f7ab34039043c2a614a1df89c
myominthein-dev <myomin9916@gmail.com>
2026-01-03 09:15:56 +0630

    supabase setup & nav-active-color

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,7 +1,20 @@
-const page = () => {
-  return (
-    <p> invoice page</p>
-  )
-}
+import { fetchLatestInvoices } from "@/app/lib/data";
+const page = async () => {
+  const datas = await fetchLatestInvoices();
 
-export default page
+  return (
+    <div>
+      {
+      datas?.map((invoice: any) => (
+        <div key={invoice.id}>
+          <p>
+            {invoice.name} - {invoice.amount}
+          </p>
+        </div>
+      ))
+      }
+    </div>
+  );
+};
+
+export default page;
--- app/seed/route.ts
+++ app/seed/route.ts
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt';
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require', prepare: false });
 
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -101,14 +101,30 @@ async function seedRevenue() {
   return insertedRevenue;
 }
 
+async function resetDatabase () {
+  await sql `TRUNCATE TABLE 
+    invoices,
+    customers,
+    users,
+    revenue
+    ReSTART IDENTITY CASCADE;
+    `;
+}
+
 export async function GET() {
   try {
-    const result = await sql.begin((sql) => [
-      seedUsers(),
-      seedCustomers(),
-      seedInvoices(),
-      seedRevenue(),
-    ]);
+    await resetDatabase();
+    // const result = await sql.begin((sql) => [
+    //   seedUsers(),
+    //   seedCustomers(),
+    //   seedInvoices(),
+    //   seedRevenue(),
+    // ]);
+
+    await seedUsers();
+    await seedCustomers();
+    await seedInvoices();
+    await seedRevenue();
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
